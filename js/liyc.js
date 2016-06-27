1e75dbebef4aa2a405abc0c7329896eeecf5a4ed
unfashionable <liyc1989@gmail.com>
2016-06-27 14:54:09 -0400

    fix mouse hover effect bug

--- js/liyc.js
+++ js/liyc.js
@@ -3,3 +3,13 @@ $(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass(
 }).on('hidden.bs.collapse', function(){
 $(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
 });
+
+// var element = document.getElementById('myElement');
+
+var element = $('.list_post_box');
+// alert(element);
+new ResizeSensor(element, function() {
+	var my_height = element.height() + 20;
+    $('.list_post_box_wrapper').height(my_height);
+});
+
